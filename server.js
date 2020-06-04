738f14d4b542e487191c2114c31d433c3fbc828c
shujo <joshue@outdoorequipped.com>
2020-06-04 17:22:30 +0800

    added client to the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const routeHandler = require('./routes/index');
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -10,5 +11,14 @@ app.use(express.static(__dirname));
 app.use(cors());
 
 routeHandler(app); //route handler
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
